d5b787876363b37d9d0b51672b8363f37056e202
Kyryl Stronko <kirstronko@gmail.com>
2020-10-01 00:33:50 +0300

    Lesson 1

---
+++ Unit 2/Demo 2/vars.js
@@ -0,0 +1,7 @@
+let user = 'Vasya';
+const admin = user;
+
+user = 'Vanya';
+
+console.log(user);
+console.log(admin);
---
+++ Unit 3/Demo 1/types-console-logs.js
@@ -0,0 +1,5 @@
+const name = 'Vasya';
+
+console.log(`Hello, I'm ${name}`);
+console.log(`Hello, I'm ${'name'}`);
+console.log(`Hello, I'm ${1}`);
