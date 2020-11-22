153adc10e0214d041125213acf45f15c246126dc
Kyryl Stronko <kirstronko@gmail.com>
2020-11-22 21:12:00 +0200

    dates

--- for-sorting/Lesson 1/unit-17/date.js
+++ for-sorting/Lesson 1/unit-17/date.js
@@ -2,5 +2,14 @@ const now = new Date();
 
 console.log(now); // Wed Nov 18 2020 23:17:42 GMT+0200
 
-const initialDate = new Date(0); // Thu Jan 01 1970 03:00:00 GMT+0300
-const initialDate = new Date(24 * 3600 * 1000); // Fri Jan 02 1970 03:00:00 GMT+0300
+// Thu Jan 01 1970 03:00:00 GMT+0300
+const initialDate = new Date(0);
+
+// Fri Jan 02 1970 03:00:00 GMT+0300
+const numDate = new Date(24 * 3600 * 1000); 
+
+// Thu Jan 26 2017 02:00:00 GMT+0200
+const srtDate = new Date("2017-01-26");
+
+// Sat Jan 01 2011 02:03:04 GMT+0200
+const numsDate = new Date(2011, 0, 1, 2, 3, 4, 567);
