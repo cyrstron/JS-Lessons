6c52fb93a430cd04b3cc353fc60abc0f12e0112c
Kyryl Stronko <Kyryl.Stronko@logianalytics.com>
2021-01-04 22:54:46 +0200

    oop

--- for-sorting/Lesson 1/unit-20/liskov-substitution.js
+++ for-sorting/Lesson 1/unit-20/liskov-substitution.js
@@ -1,31 +1,20 @@
-class Customer {
-    cart = [];
+class ThermalController {
+    constructor(isOn) {
 
-    constructor(http, id, userName) {
-        this._http = http;
-        this.id = id;
-        this.userName = userName;
+    }
+    
+    on() {
+        /*...*/
     }
 
-    buy() {
-        return this._http.buyPurchases(this.id, this.cart);
+    off() {
+        /*...*/
     }
 }
 
-class GoldCustomer extends Customer {
-    constructor(http, id, userName) {
-        super(http, id, userName);
+class AutoThermalController  {
+    constructor(temp) {
+        this.
 
-        this.discount = 0.1;
-    }
-
-    buyWithDiscount(purchases) {
-        return this._http.buyPurchases(this.id, purchases, {
-            discount: this.discount,
-        });
-    }
-
-    buy() {
-        console.log('You have a discount! Feel free to use it!');
     }
 }
